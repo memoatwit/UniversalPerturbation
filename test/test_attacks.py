dc72f84a9a1994e681b79a736c0295016e1ba224
phatgd <phatduong1011@gmail.com>
2024-04-02 09:00:53 -0700

    git separated test fixture

--- test/test_attacks.py
+++ test/test_attacks.py
@@ -1,37 +1,14 @@
 import numpy as np
-from PIL import Image
-import tensorflow as tf
-from image_processing import get_mobilenetv2_classifier
 
 from cleverhans.tf2.attacks.fast_gradient_method import fast_gradient_method as tf_fgsm
 from cleverhans.tf2.attacks.basic_iterative_method import basic_iterative_method as tf_bim
 from tf_attack import generate_fgsm_perturbation, generate_iterative_perturbation, generate_universal_perturbation
-
-import pytest
-
-@pytest.fixture()
-def test_image_tf() -> tf.Tensor:
-    image = np.random.rand(224,224,3) * 255
-    image = tf.convert_to_tensor(image, dtype=tf.float32)
-    return image[None, ...]
-
-@pytest.fixture()
-def test_images_tf(test_image_tf) -> list[tf.Tensor]:
-    return [test_image_tf] * 10
-    
-@pytest.fixture()
-def tf_classifier():
-    model, _ = get_mobilenetv2_classifier()
-    return model
-
-@pytest.fixture(params=[0, 0.005, 0.01, 0.03, 0.05, 0.1, 0.15, 0.3, 0.5])
-def eps():
-    pass
-        
+       
 def test_cleverhans_tf_attacks(test_image_tf, tf_classifier):
     fgsm_noise = tf_fgsm(tf_classifier, test_image_tf, eps=1, norm = np.inf)
     bim_noise = tf_bim(tf_classifier, test_image_tf, eps=1, eps_iter=1, nb_iter=1, norm = np.inf)
 
+    assert bim_noise is not None
     assert fgsm_noise is not None
 
 def test_custom_tf_attacks(test_images_tf, tf_classifier):
